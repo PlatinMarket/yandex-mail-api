19c6a0ef5925dc5c54ef3bb31fcc24ec674e4864
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2022-08-08 15:50:49 +0300

    Router pass custom token

--- app/api/router.php
+++ app/api/router.php
@@ -28,8 +28,8 @@ Platin\Util\Router::route('/logged/user', function($App){
 Platin\Util\Router::route('/(.*?)', function($App){
   if (!Platin\Lib\Configure::read("Security.Allow")) Platin\Lib\Configure::write("Security.Allow", "@@@");
   if ($App->Request->fromLocal()) return false;
-  if (PlatinBox\OpenId::logged() || strpos($App->Request->clientIp(), Platin\Lib\Configure::read("Security.Allow")) !== false) return false; 
-  
+  if (PlatinBox\OpenId::logged() || strpos($App->Request->clientIp(), Platin\Lib\Configure::read("Security.Allow")) !== false) return false;
+
   http_response_code(401);
 
   if ($App->Request->isAjax()){
@@ -37,26 +37,37 @@ Platin\Util\Router::route('/(.*?)', function($App){
     echo json_encode(array("code" => 401, "message" => "Unauthorized"));
     return;
   }
-  
+
   header("Content-Type: text/plain");
   echo "401 - Unauthorized";
 });
 
+// Yandex TOKEN List
+Platin\Util\Router::route("/token/list", function($app) {
+	header("Content-Type: application/json");
+	echo json_encode($app->Configure->read("YandexMail.Token"));
+	return;
+});
+
+
 // Yandex Mail Interface
 Platin\Util\Router::route('/(.*?)/(.*?)', function($App, $command, $action){
-
   $action = substr($action, -1) == '/' ? substr($action, 0, strlen($action) - 1) : $action;
   $action = strtolower($action);
   $command = ucfirst(strtolower($command));
   $command_class = 'YandexMail\\' . $command;
   if (strrpos($action, '/') !== false || !class_exists($command_class)) return false;
-  $command_class = new $command_class($App->Configure->read("YandexMail.Token"));
-  if (!method_exists($command_class, $action)) return false;
-  
-  header('Content-Type: application/json');
-
   $params = json_decode(file_get_contents('php://input'), true);
   $params = is_array($params) ? $params : array();
+
+  if(!isset($params["token"])) $params["token"] = "TODO_DEFAULT_TOKEN";
+
+  $command_class = new $command_class($params["token"]);
+  unset($params["token"]);
+
+  if (!method_exists($command_class, $action)) return false;
+
+  header('Content-Type: application/json');
 
   if (strtolower($command) == "domain" && strtolower($action) == "delete" && isset($params['domain']) && $params['domain'] == $App->Configure->read("YandexMail.Base")) {
     echo json_encode(array('success' => 'error', 'error' => 'Base domain cannot be delete!'));
